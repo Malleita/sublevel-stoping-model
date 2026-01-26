2d30a8304cfe19c3a917473e7bfadbc9b03a64f4
Diego Mallea <diego.mallea@usm.cl>
2026-01-26 14:45:45 -0300

    Update prueba_sim.py

--- prueba_sim.py
+++ prueba_sim.py
@@ -4,10 +4,11 @@ import random
 import gurobipy as gp
 from gurobipy import GRB
 
+
 m = gp.Model("obj_ejemplo")
 
 cas = 1   #caserones +1
-sub_c = 3   #subcaserones +1
+sub_c = 1   #subcaserones +1
 #Parametros manuales en este caso para un caseron de 3 sub
 
 #======= CONJUNTOS ==========
@@ -18,9 +19,9 @@ C = range(cas)
 #subcaserones
 S = {c: range(sub_c*c,sub_c*(c+1)) for c in C}
 #ultimo de los subcaserones
-US = {c: (3*(c+1) - 1) for c in C}
+#US = {c: (3*(c+1) - 1) for c in C}
 #chimeneas del caseron c
-CH = {c: range(sub_c) for c in C}
+#CH = {c: range(sub_c) for c in C}
 #presedencias
 P_c = {
     0: None,
@@ -60,19 +61,10 @@ time_l = 15
 T = range(time_l)
 range(1,time_l)
 #======= PARAMETROS ======
-for k in C:
-    Mass_drill = {
-        c: random.randint(1,5)
-        for c in S[k]
-    }
-    Mass_blast = {
-        c: random.randint(1,5)
-        for c in S[k]
-    }
-    Mass_extr = {
-        c: random.randint(1,5)
-        for c in S[k]
-    }
+Mass_drill = {c: random.randint(1,5) for n in C for c in S[n]}
+Mass_blast = {c: random.randint(1,5) for n in C for c in S[n]}
+Mass_extr  = {c: random.randint(1,5) for n in C for c in S[n]}
+
 Lambda = {
     "i": 1.0,
     "j": 1.0,
@@ -83,6 +75,12 @@ Lambda = {
 Mass_drill_B = {b: random.randint(1, 5) for n in C for b in B[n]}
 Mass_blast_B = {b: random.randint(1, 5) for n in C for b in B[n]}
 Mass_extr_B  = {b: random.randint(1, 5) for n in C for b in B[n]}
+
+# Masas para galerías 
+Mass_drill_G = {g: random.randint(1,5) for n in C for g in list(G_1[n]) + list(G_2[n])}
+Mass_blast_G = {g: random.randint(1,5) for n in C for g in list(G_1[n]) + list(G_2[n])}
+Mass_extr_G  = {g: random.randint(1,5) for n in C for g in list(G_1[n]) + list(G_2[n])}
+
 
 #duracion del periodo
 T_t = {
@@ -178,232 +176,168 @@ for k in K:
 
 for c in C:
     for t in T:
-        m.addConstr((gp.quicksum(
-            T_t[t] * (
-                gp.quicksum(x_hat[i,m,t,0] for i in I_h) +
-                gp.quicksum(y_hat[j,m,t]   for j in J)   +
-                gp.quicksum(z_hat[k,m,t]   for k in K)
+        m.addConstr(
+            gp.quicksum(
+                T_t[t] * (
+                    gp.quicksum(x_hat[ih, g, t, 0] for ih in I_h) +
+                    gp.quicksum(y_hat[j,  g, t]    for j  in J)   +
+                    gp.quicksum(z_hat[k,  g, t]    for k  in K)
+                )
+                for g in G_1[c]
             )
-            for m in G_1(c)
-        ) + 
-        gp.quicksum(
-            T_t[t] * (
-                gp.quicksum(x_hat[i,n,t,0] for i in I_h) +
-                gp.quicksum(y_hat[j,n,t]   for j in J)   +
-                gp.quicksum(z_hat[k,n,t]   for k in K)
+            +
+            gp.quicksum(
+                T_t[t] * (
+                    gp.quicksum(x_hat[ih, g, t, 0] for ih in I_h) +
+                    gp.quicksum(y_hat[j,  g, t]    for j  in J)   +
+                    gp.quicksum(z_hat[k,  g, t]    for k  in K)
+                )
+                for g in G_2[c]
             )
-            for n in G_2(c)
-        ) +
-        gp.quicksum(
-            T_t[t] * (
-                gp.quicksum(x_hat[i,nu,t,2] for i in I_h) +
-                gp.quicksum(y_hat[j,nu,t]   for j in J)   +
-                gp.quicksum(z_hat[k,nu,t]   for k in K)
+            +
+            gp.quicksum(
+                T_t[t] * (
+                    gp.quicksum(x_hat[idr, nu, t, 2] for idr in I_d) +
+                    gp.quicksum(y_hat[j,   nu, t]    for j   in J)   +
+                    gp.quicksum(z_hat[k,   nu, t]    for k   in K)
+                )
+                for nu in S[c]
             )
-            for nu in S[c]
-        ) +
-        gp.quicksum(
-            T_t[t] * (
-                gp.quicksum(x_hat[i,b,t,2] for i in I_h) +
-                gp.quicksum(y_hat[j,b,t]   for j in J)   +
-                gp.quicksum(z_hat[k,b,t]   for k in K)
-            )
-            for b in B[c]
-        ) +
-        gp.quicksum(
-            T_t[t] * (
-                gp.quicksum(x_hat[i,l,t,2] for i in I_h) +
-                gp.quicksum(y_hat[j,l,t]   for j in J)   +
-                gp.quicksum(z_hat[k,l,t]   for k in K)
-            )
-            for l in CH[c]
-        ))
-        <= T_t[t]
-        ) 
+            <= T_t[t]
+        )
 
 #---- Progreso acumulado ------
 
 for n in C:
-    for nu in S[n]:
-        for c in G_1[nu]:
-            for t in T:
-                m.addConstr(
-                    x_bar[c,t,0]*Mass_drill[c] <= gp.quicksum(x_hat[i,c,tau,0] * T_t[tau] for i in I_h for tau in range(0,t+1))
-                )
-
-for n in C:
-    for nu in S[n]:
-        for c in G_2[nu]:
-            for t in T:
-                m.addConstr(
-                    x_bar[c,t,0]*Mass_drill[c] <= gp.quicksum(x_hat[i,c,tau,0] * T_t[tau] for i in I_h for tau in range(0,t+1))
-                )
-
-for n in C:
-    for nu in S[n]:
-        for c in G_1[nu]:
-            for t in T:
-                m.addConstr(
-                    y_bar[c,t]*Mass_blast[c] <= gp.quicksum(y_hat[j,c,tau] * T_t[tau] for j in J for tau in range(0,t+1))
-                )
-
-for n in C:
-    for nu in S[n]:
-        for c in G_2[nu]:
-            for t in T:
-                m.addConstr(
-                    y_bar[c,t]*Mass_blast[c] <= gp.quicksum(y_hat[j,c,tau] * T_t[tau] for j in J for tau in range(0,t+1))
-                )
-
-for n in C:
-    for nu in S[n]:
-        for c in G_1[nu]:
-            for t in T:
-                m.addConstr(
-                    z_bar[c,t]*Mass_extr[c] <= gp.quicksum(z_hat[k,c,tau] * T_t[tau] for k in K for tau in range(0,t+1))
-                )
-
-for n in C:
-    for c in S[n]:
+    for c in G_1[n]:
         for t in T:
             m.addConstr(
-                x_bar[c,t,2] * Mass_drill[c] <= gp.quicksum(x_hat[i,c,tau,2] * T_t[tau] for i in I_d for tau in range(0,t+1))
+                x_bar[c,t,0] * Mass_drill_G[c]
+                <= gp.quicksum(
+                    x_hat[i,c,tau,0] * T_t[tau]
+                    for i in I_h
+                    for tau in range(0, t+1)
+                )
+            )
+
+            m.addConstr(
+                y_bar[c,t] * Mass_blast_G[c]
+                <= gp.quicksum(
+                    y_hat[j,c,tau] * T_t[tau]
+                    for j in J
+                    for tau in range(0, t+1)
+                )
+            )
+
+            m.addConstr(
+                z_bar[c,t] * Mass_extr_G[c]
+                <= gp.quicksum(
+                    z_hat[k,c,tau] * T_t[tau]
+                    for k in K
+                    for tau in range(0, t+1)
+                )
+            )
+
+for n in C:
+    for c in G_2[n]:
+        for t in T:
+            m.addConstr(
+                x_bar[c,t,0] * Mass_drill_G[c]
+                <= gp.quicksum(
+                    x_hat[i,c,tau,0] * T_t[tau]
+                    for i in I_h
+                    for tau in range(0, t+1)
+                )
+            )
+
+            m.addConstr(
+                y_bar[c,t] * Mass_blast_G[c]
+                <= gp.quicksum(
+                    y_hat[j,c,tau] * T_t[tau]
+                    for j in J
+                    for tau in range(0, t+1)
+                )
+            )
+
+            m.addConstr(
+                z_bar[c,t] * Mass_extr_G[c]
+                <= gp.quicksum(
+                    z_hat[k,c,tau] * T_t[tau]
+                    for k in K
+                    for tau in range(0, t+1)
+                )
             )
 
 for n in C:
     for c in S[n]:
         for t in T:
             m.addConstr(
-                y_bar[c,t] * Mass_blast[c] <= gp.quicksum(y_hat[j,c,tau] * T_t[tau] for j in J for tau in range(0,t+1))
+                x_bar[c,t,2] * Mass_drill[c]
+                <= gp.quicksum(
+                    x_hat[i,c,tau,2] * T_t[tau]
+                    for i in I_d
+                    for tau in range(0, t+1)
+                )
             )
 
-for n in C:
-    for c in S[n]:
-        for t in T:
             m.addConstr(
-                z_bar[c,t] * Mass_extr[c] <= gp.quicksum(z_hat[k,c,tau] * T_t[tau] for k in K for tau in range(0,t+1))
+                y_bar[c,t] * Mass_blast[c]
+                <= gp.quicksum(
+                    y_hat[j,c,tau] * T_t[tau]
+                    for j in J
+                    for tau in range(0, t+1)
+                )
+            )
+
+            m.addConstr(
+                z_bar[c,t] * Mass_extr[c]
+                <= gp.quicksum(
+                    z_hat[k,c,tau] * T_t[tau]
+                    for k in K
+                    for tau in range(0, t+1)
+                )
             )
 
 #---- Restricciones de vinculo y finalizacion ------
 
-# Esta muy rara la notacion por el S_n
 for n in C:
     for c in G_1[n]:
-        m.addConstr(
-            1 == gp.quicksum(x[c,t,0]) for t in T
-        )
+        m.addConstr(gp.quicksum(x[c,t,0] for t in T) == 1)
+        m.addConstr(gp.quicksum(y[c,t]   for t in T) == 1)
+        m.addConstr(gp.quicksum(z[c,t]   for t in T) == 1)
 
 for n in C:
     for c in G_2[n]:
-        m.addConstr(
-            1 == gp.quicksum(x[c,t,0]) for t in T
-        )
-
-for n in C:
-    for c in G_1[n]:
-        m.addConstr(
-            1 == gp.quicksum(y[c,t]) for t in T
-        )
-
-for n in C:
-    for c in G_1[n]:
-        m.addConstr(
-            1 == gp.quicksum(y[c,t]) for t in T
-        )
-
-for n in C:
-    for c in G_1[n]:
-        m.addConstr(
-            1 == gp.quicksum(z[c,t]) for t in T
-        )
-
-for n in C:
-    for c in G_1[n]:
-        m.addConstr(
-            1 == gp.quicksum(z[c,t]) for t in T
-        )
+        m.addConstr(gp.quicksum(x[c,t,0] for t in T) == 1)
+        m.addConstr(gp.quicksum(y[c,t]   for t in T) == 1)
+        m.addConstr(gp.quicksum(z[c,t]   for t in T) == 1)
 
 for n in C:
     for c in S[n]:
-        m.addConstr(
-            1 == gp.quicksum(x[c,t,2]) for t in T
-        )
-
-for n in C:
-    for c in S[n]:
-        m.addConstr(
-            1 == gp.quicksum(y[c,t]) for t in T
-        )
-
-for n in C:
-    for c in S[n]:
-        m.addConstr(
-            1 == gp.quicksum(z[c,t]) for t in T
-        )
+        m.addConstr(gp.quicksum(x[c,t,2] for t in T) == 1)
+        m.addConstr(gp.quicksum(y[c,t]   for t in T) == 1)
+        m.addConstr(gp.quicksum(z[c,t]   for t in T) == 1)
 
 for n in C:
     for c in G_1[n]:
         for t in T:
-            m.addConstr(
-                x[c,t,0] <= x_bar[c,t,0]
-            )
+            m.addConstr(x[c,t,0] <= x_bar[c,t,0])
+            m.addConstr(y[c,t]   <= y_bar[c,t])
+            m.addConstr(z[c,t]   <= z_bar[c,t])
 
 for n in C:
     for c in G_2[n]:
         for t in T:
-            m.addConstr(
-                x[c,t,0] <= x_bar[c,t,0]
-            )
-
-for n in C:
-    for c in G_1[n]:
-        for t in T:
-            m.addConstr(
-                y[c,t] <= y_bar[c,t]
-            )
-
-for n in C:
-    for c in G_2[n]:
-        for t in T:
-            m.addConstr(
-                y[c,t] <= y_bar[c,t]
-            )
-
-for n in C:
-    for c in G_1[n]:
-        for t in T:
-            m.addConstr(
-                z[c,t] <= z_bar[c,t]
-            )
-
-for n in C:
-    for c in G_2[n]:
-        for t in T:
-            m.addConstr(
-                z[c,t] <= z_bar[c,t]
-            )
+            m.addConstr(x[c,t,0] <= x_bar[c,t,0])
+            m.addConstr(y[c,t]   <= y_bar[c,t])
+            m.addConstr(z[c,t]   <= z_bar[c,t])
 
 for n in C:
     for c in S[n]:
-        for t in t:
-            m.addConstr(
-                x[c,t,2] <= x_bar[c,t,2]
-            )
-
-#estas creo que no son necesarias --------
-for n in C:
-    for c in S[n]:
-        for t in t:
-            m.addConstr(
-                y[c,t] <= y_bar[c,t]
-            )
-
-for n in C:
-    for c in S[n]:
-        for t in t:
-            m.addConstr(
-                z[c,t] <= z_bar[c,t]
-            )
+        for t in T:
+            m.addConstr(x[c,t,2] <= x_bar[c,t,2])
+            m.addConstr(y[c,t]   <= y_bar[c,t])
+            m.addConstr(z[c,t]   <= z_bar[c,t])
 
 #======= Secuencias de procesos =======
 
@@ -449,54 +383,74 @@ for n in C:
                 gp.quicksum(z_hat[k,c,t] for k in K) <= J_val * y_bar[c,t - 1]
             )
 
-#==== Precedencia estricta ======
+# ---- Precedencia estricta ----
 
 for n in C:
     for c in G_1[n]:
-        for t in range(1,time_l):
-            for m in P_c:
-                if P_c[m] is None:
+        for t in range(1, time_l):
+            for pred in P_c:
+                if P_c[pred] is None:
                     continue
+
                 m.addConstr(
-                    gp.quicksum(x_hat[i,c,t,0] for i in I_h) <= I_h * z_bar[m, t -1]
+                    gp.quicksum(x_hat[i,c,t,0] for i in I_h)
+                    <= len(I_h) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(y_hat[j,c,t] for j in J) <= J * z_bar[m, t -1]
+                    gp.quicksum(y_hat[j,c,t] for j in J)
+                    <= len(J) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(z_hat[k,c,t] for k in K) <= K * z_bar[m, t -1]
+                    gp.quicksum(z_hat[k,c,t] for k in K)
+                    <= len(K) * z_bar[pred, t-1]
                 )
+
 
 for n in C:
     for c in G_2[n]:
-        for t in range(1,time_l):
-            for m in P_c:
-                if P_c[m] is None:
+        for t in range(1, time_l):
+            for pred in P_c:
+                if P_c[pred] is None:
                     continue
+
                 m.addConstr(
-                    gp.quicksum(x_hat[i,c,t,0] for i in I_h) <= I_h * z_bar[m, t -1]
+                    gp.quicksum(x_hat[i,c,t,0] for i in I_h)
+                    <= len(I_h) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(y_hat[j,c,t] for j in J) <= J * z_bar[m, t -1]
+                    gp.quicksum(y_hat[j,c,t] for j in J)
+                    <= len(J) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(z_hat[k,c,t] for k in K) <= K * z_bar[m, t -1]
+                    gp.quicksum(z_hat[k,c,t] for k in K)
+                    <= len(K) * z_bar[pred, t-1]
                 )
+
 
 for n in C:
     for c in S[n]:
-        for t in range(1,time_l):
-            for m in P_c:
-                if P_c[m] is None:
+        for t in range(1, time_l):
+            for pred in P_c:
+                if P_c[pred] is None:
                     continue
+
                 m.addConstr(
-                    gp.quicksum(x_hat[i,c,t,2] for i in I_h) <= I_h * z_bar[m, t -1]
+                    gp.quicksum(x_hat[i,c,t,2] for i in I_d)
+                    <= len(I_d) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(y_hat[j,c,t] for j in J) <= J * z_bar[m, t -1]
+                    gp.quicksum(y_hat[j,c,t] for j in J)
+                    <= len(J) * z_bar[pred, t-1]
                 )
+
                 m.addConstr(
-                    gp.quicksum(z_hat[k,c,t] for k in K) <= K * z_bar[m, t -1]
+                    gp.quicksum(z_hat[k,c,t] for k in K)
+                    <= len(K) * z_bar[pred, t-1]
                 )
 
 #----- Restricciones del objetivo -------
@@ -694,3 +648,36 @@ for c in C:
     m.addConstr(
         gp.quicksum(zC[c, t] for t in T) == 1
     )
+
+# =======================
+#   FUNCIÓN OBJETIVO 
+# =======================
+
+m.setObjective(
+    gp.quicksum(t * zC[c, t] for c in C for t in T),
+    GRB.MINIMIZE
+)
+
+m.optimize()
+
+print("\n=== RESULTADO ===")
+print("Status:", m.Status)
+
+if m.Status == GRB.OPTIMAL:
+    print("ObjVal:", m.ObjVal)
+
+    for c in C:
+        t_end = [t for t in T if zC[c, t].X > 0.5]
+        print(f"Caserón {c} termina en t =", t_end[0] if t_end else None)
+
+elif m.Status == GRB.INFEASIBLE:
+    print("Modelo INFEASIBLE. Calculando IIS...")
+    m.computeIIS()
+    m.write("iis.ilp")
+    print("Escribí iis.ilp (abre ese archivo para ver restricciones conflictivas).")
+
+elif m.Status == GRB.UNBOUNDED:
+    print("Modelo UNBOUNDED (sin cota). Revisa objetivo/restricciones.")
+else:
+    print("Estado distinto (p.ej. TIME_LIMIT).")
+
